dbc089d675559d15bfd44c79870bbbf3c143f032
WiseDoge <wisedoge@outlook.com>
2017-12-14 12:06:25 +0800

    Add Test.

--- python/test.py
+++ python/test.py
@@ -16,5 +16,7 @@ def test_kd_tree(n_samples, n_features=5):
 
 if __name__ == '__main__':
     for _ in range(500):
-        test_kd_tree(200, 10)
+        test_kd_tree(300, 10)
+    for _ in range(500):
+        test_kd_tree(300, 30)
     print("Success!")
